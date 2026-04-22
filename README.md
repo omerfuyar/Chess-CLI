3ff44d6ac5b89eeb56893b693215dce8ba6a4d8c
omerfuyar <denemeyo@mail.com>
2026-04-23 01:34:04 +0300

    -

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chess-CLI
+Chess-CLI is a turn based P2P (tbd) chess engine.
---
+++ shuild.c
@@ -0,0 +1,10 @@
+#define SHUILD_IMPLEMENTATION
+#define SHUC_ENABLE_INCREMENTAL
+#define SHUC_NO_RUN_LOG
+#define SHUC_SHORT_LOG
+#include "dependencies/shuild/shuild.h"
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
