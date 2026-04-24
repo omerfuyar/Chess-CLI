7982e78847cc1b1d6853ce8acf0fcb925349ed2e
omerfuyar <denemeyo@gmail.com>
2026-04-24 17:46:25 +0300

    -shunei issues

--- shuild.c
+++ shuild.c
@@ -9,6 +9,18 @@ int main(int argc, char **argv)
     SHU_CompilerTryConfigure("gcc");
     SHU_UtilAutomate(argc, argv);
 
+    SHU_CompilerAddFlags(SHUM_FLAGS_STANDARD_C23 " -static -fno-stack-protector");
+
+    if (argc > 1) // debug
+    {
+        SHU_CompilerAddFlags(SHUM_FLAGS_WARNING_HIGH SHUM_FLAGS_DEBUG);
+        // SHU_CompilerAddFlags(SHUM_FLAGS_DEBUG SHUM_FLAGS_WARNING_ERROR " -Wno-missing-declarations -Wno-switch -Wno-format-truncation -Wno-unused-function -DDEBUG -Wno-format-nonliteral");
+    }
+    else // release
+    {
+        SHU_CompilerAddFlags(SHUM_FLAGS_OPTIMIZATION_HIGH);
+    }
+
     SHU_ModuleBegin("Chess-CLI", NULL);
     SHU_ModuleAddSourceFile("src/");
     SHU_ModuleAddIncludeDirectory("include/");
